f6ea8ddcf35699eb55268ba4081cce9f95bcf3e1
Rémy BEULE-DAUZAT <remy.b3d@gmail.com>
2016-11-08 16:39:10 +0200

    correct regress : now it use the fit on all the training set before the evaluation of the test set add the linear hand made to the CV

--- regression/Regress.py
+++ regression/Regress.py
@@ -3,11 +3,13 @@ from sklearn import linear_model;
 from sklearn.gaussian_process import GaussianProcessRegressor;
 from sklearn.kernel_ridge import KernelRidge;
 from sklearn.cross_validation import ShuffleSplit
+from regression.LinearRegression import LinearRegression
 
 
-def report_performance(mean_train_mse,mean_val_mse,  classifiers):
-    for i in range(0, len(classifiers)):
-        print(classifiers[i]['name'])
+
+def report_performance(mean_train_mse,mean_val_mse,  regressors):
+    for i in range(0, len(regressors)):
+        print(regressors[i]['name'])
         print("\tmean mse for training =",mean_train_mse[i])
         print("\tmean mse for validation =",mean_val_mse[i])
 
@@ -16,10 +18,10 @@ def mse(X, y, reg):
     return np.mean((reg.predict(X) - y) ** 2);
 
 
-def get_errors(classifier, X_train, y_train, X_val, y_val):
-    reg = classifier.fit(X_train, y_train)
-    mse_train = mse(X_train,y_train,reg)
-    mse_val = mse(X_val,y_val,reg)
+def get_errors(regressor, X_train, y_train, X_val, y_val):
+    regressor.fit(X_train, y_train)
+    mse_train = mse(X_train,y_train,regressor)
+    mse_val = mse(X_val,y_val,regressor)
     return mse_train, mse_val;
 
 
@@ -37,20 +39,21 @@ X_data = data[:, range(1, indexVote)]
 y_data = data[:, indexVote]
 
 # Setup Classifiers
-linear = {'classifier': linear_model.LinearRegression(), 'name': "LinearRegression"}
-lasso = {'classifier': linear_model.LassoCV(), 'name': "LassoRegression"}
-ridge = {'classifier': linear_model.Ridge(), 'name': "RidgeRegression"}
-ridgeCV = {'classifier': linear_model.RidgeCV(), 'name': "RidgeRegressionCV"}
-gaussian = {'classifier': GaussianProcessRegressor(), 'name': "GaussianProcessRegression"}
-kernel = {'classifier': KernelRidge(), 'name': "KernelRidgeRegression"}
+linear = {'regressor': linear_model.LinearRegression(), 'name': "LinearRegression"}
+linearHand = {'regressor': LinearRegression(), 'name': "LinearHandMadeRegression"}
+lasso = {'regressor': linear_model.LassoCV(), 'name': "LassoRegression"}
+ridge = {'regressor': linear_model.Ridge(), 'name': "RidgeRegression"}
+ridgeCV = {'regressor': linear_model.RidgeCV(), 'name': "RidgeRegressionCV"}
+gaussian = {'regressor': GaussianProcessRegressor(), 'name': "GaussianProcessRegression"}
+kernel = {'regressor': KernelRidge(), 'name': "KernelRidgeRegression"}
 
-classifiers = [linear, lasso, ridge, ridgeCV, gaussian, kernel]
+regressors = [linear, linearHand, lasso, ridge, ridgeCV, gaussian, kernel]
 
 
 
-sum_train_mse = [0] * len(classifiers)
-sum_val_mse = [0] * len(classifiers)
-nb_iter = 200
+sum_train_mse = [0] * len(regressors)
+sum_val_mse = [0] * len(regressors)
+nb_iter = 10
 # Random repartition for training & validation data in order to perform cross validation
 rs = ShuffleSplit(len(data[:, 0]), n_iter=nb_iter, test_size=0.25)
 i = 1;
@@ -62,34 +65,40 @@ for train_indexes, val_indexes in rs:
     X_train_data, y_train_data = get_x_y(train_data, indexVote)
     X_valid_data, y_valid_data = get_x_y(valid_data, indexVote)
 
-    # Compute mse error for each classifier
-    for classifier_index, item in enumerate(classifiers):
-        classifier = classifiers[classifier_index]['classifier']
-        mse_train, mse_val = get_errors(classifier, X_train_data, y_train_data, X_valid_data, y_valid_data)
-        sum_train_mse[classifier_index] += mse_train
-        sum_val_mse[classifier_index] += mse_val
+    # Compute mse error for each regressor
+    for regressor_index, item in enumerate(regressors):
+        regressor = regressors[regressor_index]['regressor']
+        mse_train, mse_val = get_errors(regressor, X_train_data, y_train_data, X_valid_data, y_valid_data)
+        sum_train_mse[regressor_index] += mse_train
+        sum_val_mse[regressor_index] += mse_val
 
 
 mean_train_mse = np.divide(sum_train_mse, nb_iter)
 mean_val_mse = np.divide(sum_val_mse, nb_iter)
 
-report_performance(mean_train_mse, mean_val_mse, classifiers)
+report_performance(mean_train_mse, mean_val_mse, regressors)
 
 
 #Get the best
-index_best_classifier = np.argmin(mean_val_mse)
-best_classifier = classifiers[index_best_classifier];
-print("\nbest classifier : ", best_classifier['name'])
-print("best classifier validation mean mse =", mean_val_mse[index_best_classifier])
+index_best_regressor = np.argmin(mean_val_mse)
+best_regressor = regressors[index_best_regressor];
+print("\nbest regressor : ", best_regressor['name'])
+print("best regressor validation mean mse =", mean_val_mse[index_best_regressor])
 
 
 '''
-# Perform best classifier on test data
+# Perform best regressor on test data
 '''
+#fit on all the training set
+best_regressor["regressor"].fit(X_data, y_data)
+mse_training = mse(X_data,y_data,best_regressor['regressor'])
+print("\nmse on all trainin set =", mse_training);
+
+#test on data set
 test = np.loadtxt("data/regression_dataset_testing.csv",dtype=int, skiprows=1, delimiter=',',);
 X_test = test[:,range(1,indexVote)];
 testSol = np.loadtxt("data/regression_dataset_testing_solution.csv",dtype=int, skiprows=1, delimiter=',',);
 y_test = testSol[:,1];
 
-mse_test = mse(X_test,y_test,best_classifier['classifier'])
+mse_test = mse(X_test,y_test,best_regressor['regressor'])
 print("\nmse on test set =", mse_test);
--- regression/linear.py
+++ regression/linear.py
@@ -1,5 +1,4 @@
 import numpy as np;
-from sklearn.model_selection import train_test_split;
 from sklearn import linear_model;
 from regression.LinearRegression import LinearRegression;
 
