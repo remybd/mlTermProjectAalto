3fb7267a7e4cc474fb6b3a505814d6122585641c
Arthur DK <arthurdekimpe@protonmail.com>
2016-11-09 15:21:33 +0200

    Added feature selection

--- classification/Classify.py
+++ classification/Classify.py
@@ -1,5 +1,5 @@
 from collections import UserString
-
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
@@ -51,40 +51,34 @@ def feature_selection(x, y, nb_features):
     return fit.support_
 
 
-def main():
-    # Read  data
-    training_features = np.loadtxt("classification_dataset_training.csv",
-                               dtype=int, skiprows=1, delimiter=',', usecols=range(1, 51),)
-    training_label = np.loadtxt("classification_dataset_training.csv",
-                                   dtype=int, skiprows=1, delimiter=',', usecols=range(51, 52), )
-    test_features = np.loadtxt("classification_dataset_testing.csv",
-                                    dtype=int, skiprows=1, delimiter=',', usecols=range(1, 51),)
-    test_label = np.loadtxt("classification_dataset_testing_solution.csv",
-                                 dtype=int, skiprows=1, delimiter=',', usecols=range(1, 2),)
-    features_names = np.loadtxt("classification_dataset_training.csv",
-                                 dtype=UserString, delimiter=',', usecols=range(1, 51),)[0, :]
+def plot(classifiers_name, accuracy_result, title):
+    x = np.linspace(0, len(classifiers_name), len(classifiers_name))
+    plt.bar(x, accuracy_result)
+    x = np.add(x, 0.35)
+    plt.xticks(x, classifiers_name)
+    plt.xlabel("Classifiers")
+    plt.ylabel("Accuracy error")
+    axes = plt.gca()
+    plt.legend(loc='upper left')
+    plt.title(title)
+    plt.show()
 
-    # Feature extraction
-    nb_features = 40
-    mask_array = feature_selection(training_features, training_label, nb_features)
 
-    # Keep best features
-    training_features = training_features[:, mask_array]
-    choosen_features = features_names[mask_array]
-    test_features = test_features[:, mask_array]
-
-    # Setup Classifiers
+def setup_classifiers():
     lr = {'classifier': LogisticRegression(), 'name': "LogisticRegression"}
     gnb = {'classifier': GaussianNB(), 'name': "GaussianNB"}
     svc = {'classifier': LinearSVC(C=1.0), 'name': "LinearSVC"}
-    my_gnb = {'classifier': GaussianNaiveBayesClassifier(), 'name': "GaussianNaiveBayesClassifier (from scratch)"}
+    my_gnb = {'classifier': GaussianNaiveBayesClassifier(), 'name': "GaussianNaiveBayesClassifier"}
     rfc = {'classifier': RandomForestClassifier(n_estimators=100), 'name': "RandomForestClassifier"}
-    classifiers = [my_gnb, gnb, svc, rfc, lr]
+    classifiers = [gnb, svc, rfc]
+    classifiers_name = []
+    for classifier in classifiers:
+        classifiers_name.append(classifier["name"])
+    return classifiers, classifiers_name
 
-    nb_iter = 10
 
-    list_total = [0] * len(classifiers)
-    list_incorrect = [0] * len(classifiers)
+def perform_cross_validation(training_features, training_label, classifiers, nb_iter):
+    list_total, list_incorrect = [0] * len(classifiers), [0] * len(classifiers)
     # Random repartition for training & validation training_data in order to perform cross validation
     rs = ShuffleSplit(len(training_features[:, 0]), n_iter=nb_iter, test_size=0.20)
     for train_indexes, valid_indexes in rs:
@@ -99,24 +93,75 @@ def main():
 
         list_total = np.add(list_total, total)
         list_incorrect = np.add(list_incorrect, incorrect)
+    return np.divide(list_incorrect, list_total)
 
-    print("Cross Validation result:")
-    report_performance(list_total, list_incorrect, classifiers)
 
-    '''
-    # Perform best classifier on test training_data
-    '''
-    accuracy_result = np.divide(list_incorrect, list_total)
-    index_best_classifier = accuracy_result.argmin()
+def plot_all(results, classifiers, title):
+    for index, classifier in enumerate(classifiers):
+        plt.plot(range(1, 51), results[:, index], label=classifier["name"])
+    plt.xlabel("Features number")
+    plt.ylabel("Accuracy error")
+    plt.legend(loc='upper right')
+    plt.title(title)
+    plt.show()
 
-    print("Best classifier is ", classifiers[index_best_classifier]['name'])
-    print("\n\nActual performance on test set:")
 
-    # Classify for each randomly picked training and validation set
-    total, incorrect = classify_all(classifiers,
-                                    training_features, training_label,
-                                    test_features, test_label)
-    report_performance(total, incorrect, classifiers)
+def main():
+    # Read  data
+    training_features = np.loadtxt("classification_dataset_training.csv",
+                               dtype=int, skiprows=1, delimiter=',', usecols=range(1, 51),)
+    training_label = np.loadtxt("classification_dataset_training.csv",
+                                   dtype=int, skiprows=1, delimiter=',', usecols=range(51, 52), )
+    test_features = np.loadtxt("classification_dataset_testing.csv",
+                                    dtype=int, skiprows=1, delimiter=',', usecols=range(1, 51),)
+    test_label = np.loadtxt("classification_dataset_testing_solution.csv",
+                                 dtype=int, skiprows=1, delimiter=',', usecols=range(1, 2),)
+    features_names = np.loadtxt("classification_dataset_training.csv",
+                                 dtype=UserString, delimiter=',', usecols=range(1, 51),)[0, :]
+
+    # Setup Classifiers
+    classifiers, classifiers_name = setup_classifiers()
+
+    test_results = np.zeros((len(training_features[0, :]), len(classifiers)))
+    valid_results = np.zeros((len(training_features[0, :]), len(classifiers)))
+    # Perform cross validation for nb features with nb in [1, 50]
+    for index, nb_features in enumerate(range(1, len(training_features[0, :]) + 1)):
+        # Feature extraction
+        mask_array = feature_selection(training_features, training_label, nb_features)
+
+        # Keep best features
+        training_features_tmp = training_features[:, mask_array]
+        # choosen_features = features_names[mask_array]
+        test_features_tmp = test_features[:, mask_array]
+
+        nb_iter = 10
+
+        accuracy_result = perform_cross_validation(training_features_tmp, training_label, classifiers, nb_iter)
+        valid_results[index] = accuracy_result
+        # print("Cross Validation result:")
+        '''
+        # report_performance(list_total, list_incorrect, classifiers)
+        # title = "Comparison between classifiers for validation accuracy error and %d features" % len(features_names)
+        # plot(classifiers_name, accuracy_result, title)
+        '''
+        # Perform best classifier on test training_data
+        '''
+        index_best_classifier = accuracy_result.argmin()
+        print("Best classifier is ", classifiers[index_best_classifier]['name'])
+        print("\n\nActual performance on test set:")
+        '''
+        # Classify for each randomly picked training and validation set
+        total, incorrect = classify_all(classifiers,
+                                        training_features_tmp, training_label,
+                                        test_features_tmp, test_label)
+        accuracy_result = np.divide(incorrect, total)
+        # report_performance(total, incorrect, classifiers)
+        # title = "Comparison between classifiers for test accuracy error and %d features" % len(features_names)
+        # plot(classifiers_name, accuracy_result, title)
+        test_results[index] = accuracy_result
+
+    plot_all(results=valid_results, classifiers=classifiers, title="Accuracy error on cross validation")
+    plot_all(results=test_results, classifiers=classifiers, title="Accuracy error on test set")
 
 if __name__ == "__main__":
     main()
